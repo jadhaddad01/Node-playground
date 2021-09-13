4d507c556c2fea256de5bf047cd4c7e6febecb81
awdad <adwadawd>
2021-09-13 14:42:56 -0400

    add simple callback example

--- 4-callbacks.js
+++ 4-callbacks.js
@@ -1,9 +1,34 @@
-setTimeout(() => {
-    console.log('2 seconds are up');
-}, 2000);
+// setTimeout(() => {
+//     console.log('2 seconds are up');
+// }, 2000);
 
-const names = ['Andrew', 'Jen', 'Jess']
-const shortNames = names.filter((name) => {
-    return name.length <= 4
+// const names = ['Andrew', 'Jen', 'Jess']
+// const shortNames = names.filter((name) => {
+//     return name.length <= 4
+// })
+// console.log(shortNames);
+
+// const geocode = (address, callback) => {
+//     setTimeout(() => {
+//         const data = {
+//             latitude: 0,
+//             longitude: 0
+//         }
+
+//         callback(data)
+//     }, 2000);
+// }
+
+// geocode('Philadelphia', (data) => {
+//     console.log(data);
+// })
+
+const add = (a, b, callback) => {
+    setTimeout(() => {
+        callback(a + b)
+    }, 2000);
+}
+
+add(1, 4, (sum) => {
+    console.log(sum)
 })
-console.log(shortNames);
