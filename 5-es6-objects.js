95603c87b44ed4c4b0ec71b9da108e93b6808e69
awdad <adwadawd>
2021-09-14 21:31:26 -0400

    function parameter destructuring

--- 5-es6-objects.js
+++ 5-es6-objects.js
@@ -17,13 +17,20 @@ const product = {
     label: 'Red Notebook',
     price: 3,
     stock: 201,
-    salePrice: undefined
+    salePrice: undefined,
+    rating: 4.2
 }
 
 // const productLabel = product.label
 // const stock = product.stock
-// const rating = undefined
+// const rating = 5 // default if no rating in product object
 
-const { label: productLabel, stock, rating } = product
-console.log(productLabel, stock);
-console.log(rating);
+// const { label: productLabel, stock, rating = 5 } = product
+// console.log(productLabel, stock);
+// console.log(rating);
+
+const transaction = (type, { label, stock }) => { // destructure in param
+    console.log(type, label, stock);
+}
+
+transaction('order', product)
